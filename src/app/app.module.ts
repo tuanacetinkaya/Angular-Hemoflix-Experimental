b527732a84e92c6ba8377aecfb9f771a003a68d1
Tuana <tuanacetinkaya7@gmail.com>
2020-10-12 10:53:24 +0300

    sidebar template and homepage in place

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent,
     HomePageComponent
   ],
   imports: [
